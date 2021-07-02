588b7bbae9447a5dca7fd5e265bd607adc8a2181
Harjot Kaur <kharjot1810@gmail.com>
2021-07-03 00:00:39 +0530

    ignored files

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
-const config = {
+require('dotenv').config();
+
+config = {
     production: {
         SECRET: process.env.SECRET,
         DATABASE: process.env.MONGODB_URI
     },
     default: {
         SECRET: 'mysecretkey',
-        DATABASE: 'mongodb+srv://Harjot1810:Harjot@1@cluster0.v8ruy.mongodb.net/AuthData?retryWrites=true&w=majority'
+        DATABASE: process.env.database
     }
 }
 

ce28600718c3f405743047a8775f083ed14960a0
Harjot Kaur <kharjot1810@gmail.com>
2021-07-11 02:23:19 +0530

    deploying again

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.post('/api/login', function (req, res) {
                     user.generateToken((err, user) => {
                         console.log(err);
                         if (err) return res.status(400).send(err);
-                        res.cookie('auth', user.token).json({
+                        res.cookie('auth', user.token, { sameSite: "none" }).json({
                             isAuth: true,
                             id: user._id
                             , email: user.email
